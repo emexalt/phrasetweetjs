24c903d69b62928e61ba2b7446af7d7e495bdac1
Em Folsom <emmeryn.dev@gmaill.com>
2019-12-13 11:49:59 -0600

    Styled HTML, reset button, all results on one page

--- phrasetweet.js
+++ phrasetweet.js
@@ -2,20 +2,28 @@
 //a javascript version of phrasetweet.py
 //em folsom, 2019, copyleft
 
+let tlOutput = "";
+let tfOutput = "";
 function main(){
     let phr1 = document.getElementById("1").value;
     let phr2 = document.getElementById("2").value;
     trimLast(phr1);
     trimFirst(phr2);
-}
+    let output = document.getElementById("result");
+    output.innerHTML= tlOutput + tfOutput;
+    tlOutput = "";
+    tfOutput = "";
+    //document.writeln(tfOutput);
+    //document.writeln('<button id="submitButton" onClick="javascript:window.location.reload(true)">Go back!</button> <link rel="stylesheet" href="main.css"></link>');
+}    
+
 
 function trimLast(string){
     for(i = string.length; i > 0; i--){
         if (string.charAt(i) == " "){
             //do nothing
         } else {
-            document.writeln(string.substr(0, i));
-            document.writeln("<BR/>");
+            tlOutput = tlOutput.concat(string.substr(0,i), "<BR/>");
         }
     }  
 }
@@ -26,8 +34,7 @@ function trimFirst(string){
         if (string.charAt(i) == " "){
             //do nothing
         } else {
-            document.writeln(string.substr(0, i));
-            document.writeln("<BR/>");
+            tfOutput = tfOutput.concat(string.substr(0,i), "<BR/>");
         }
     }  
 }
