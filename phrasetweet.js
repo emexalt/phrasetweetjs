0ae47b86918617720fefa824df519122af6d5c7f
Em Folsom <emmeryn.dev@gmail.com>
2020-08-08 21:52:14 -0500

    minor code format changes

--- phrasetweet.js
+++ phrasetweet.js
@@ -4,45 +4,45 @@
 
 let tlOutput = "";
 let tfOutput = "";
-function main(){
+function main() {
     let phr1 = document.getElementById("1").value;
     let phr2 = document.getElementById("2").value;
     let validationResult = validate(phr1, phr2);
-    if (validationResult){
+    if (validationResult) {
         trimLast(phr1);
         trimFirst(phr2);
         let output = document.getElementById("result");
-        output.innerText= tlOutput + tfOutput;
+        output.innerText = tlOutput + tfOutput;
     } else {
         let output = document.getElementById("result");
-        output.innerText= '<p id="errorText">First characters of strings do not match.</p>';
-    }   
+        output.innerText = '<p id="errorText">First characters of strings do not match.</p>';
+    }
     tlOutput = "";
     tfOutput = "";
 }
 
 
-function trimLast(string){
-    for(i = string.length; i > 0; i--){
-        if (string.charAt(i) == " "){
+function trimLast(string) {
+    for (i = string.length; i > 0; i--) {
+        if (string.charAt(i) == " ") {
             //do nothing
         } else {
-            tlOutput = tlOutput.concat(string.substr(0,i), "\n");
+            tlOutput = tlOutput.concat(string.substr(0, i), "\n");
         }
-    }  
+    }
 }
 
 
-function trimFirst(string){
-    for(i = 1; i < string.length+1; i++){
-        if (string.charAt(i) == " "){
+function trimFirst(string) {
+    for (i = 1; i < string.length + 1; i++) {
+        if (string.charAt(i) == " ") {
             //do nothing
         } else {
-            tfOutput = tfOutput.concat(string.substr(0,i), "\n");
+            tfOutput = tfOutput.concat(string.substr(0, i), "\n");
         }
-    }  
+    }
 }
 
-function validate(str1, str2){
+function validate(str1, str2) {
     return str1.charAt(0) == str2.charAt(0);
 }
